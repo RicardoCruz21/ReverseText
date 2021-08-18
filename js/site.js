e5ed64c587293903570837273a8000128bbfe0b4
RicardoCruz21 <abstergo21@gmail.com>
2021-08-17 22:38:42 -0500

    wrote reverse string functions

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hellow World");
+//Get the string from the page
+//controller function
+function getString(){
+    
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+//logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    //Reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];        
+    }
+
+    return revString;
+}
+
+//Display the Reversed string to the page
+//view function
+function displayString(revString){
+
+    //write the string to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
